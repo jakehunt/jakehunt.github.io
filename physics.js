556617e7df5784ed7508ebdccb0f95c8971afd5d
Jake <jhunt0398@gmail.com>
2018-08-12 18:56:42 +1000

    Can I get another page on this website

--- physics.js
+++ physics.js
@@ -2,7 +2,7 @@
     var sketchProc = function(processingInstance) {
      with (processingInstance) {
         size(400, 400); 
-        frameRate(20);
+        frameRate(30);
         
         // ProgramCodeGoesHere
 								/*****
